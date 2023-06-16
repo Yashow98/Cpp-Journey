aebd90991535a50d6150c536742b924f11ed7da6
Yashow98 <yashowhoo@outlook.com>
2023-06-16 22:05:10 +0800

    Add file via upload.

---
+++ Basic/data-type.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "char = " << sizeof(char) << std::endl;
+    std::cout << "int = " << sizeof(int) << std::endl;
+    std::cout << "short = " << sizeof(short) << std::endl;
+    std::cout << "long = " << sizeof(long) << std::endl;
+    std::cout << "long long = " << sizeof(long long) << std::endl;
+    std::cout << "int32_t: " << sizeof(int32_t) << std::endl;
+    std::cout << "int64_t: " << sizeof(int64_t) << std::endl;
+     
+
+    return 0;
+
+}
